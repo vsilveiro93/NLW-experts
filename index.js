75a407d7503686d69b8eba833160434f00ce3c90
Victor Silvério <158054240+vsilveiro93@users.noreply.github.com>
2024-02-14 20:45:04 -0300

    Quiz Naruto

--- index.js
+++ index.js
@@ -1,96 +1,96 @@
 const perguntas = [
-    {
-      pergunta: "Qual é a forma correta de declarar uma variável em JavaScript?",
-      respostas: [
-        "var myVar = 10;",
-        "variable myVar = 10;",
-        "let myVar = 10;"
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "Qual é a saída do seguinte código em JavaScript: console.log(typeof([]));",
-      respostas: [
-        "object",
-        "array",
-        "undefined"
-      ],
-      correta: 0
-    },
-    {
-      pergunta: "O que o método 'toFixed()' faz em JavaScript?",
-      respostas: [
-        "Arredonda um número para o inteiro mais próximo",
-        "Arredonda um número para o número especificado de casas decimais",
-        "Arredonda um número para baixo para o inteiro mais próximo"
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual é a forma correta de escrever um comentário de uma linha em JavaScript?",
-      respostas: [
-        "/* Este é um comentário */",
-        "// Este é um comentário",
-        "<!-- Este é um comentário -->"
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual dos seguintes é um tipo de dados primitivo em JavaScript?",
-      respostas: [
-        "Object",
-        "String",
-        "Function"
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual é o operador usado para comparar o valor e o tipo em JavaScript?",
-      respostas: [
-        "==",
-        "===",
-        "="
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual é o resultado da expressão '3' + 2 em JavaScript?",
-      respostas: [
-        "5",
-        "32",
-        "error"
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual é o método em JavaScript usado para remover o último elemento de um array?",
-      respostas: [
-        "pop()",
-        "removeLast()",
-        "deleteLast()"
-      ],
-      correta: 0
-    },
-    {
-      pergunta: "Qual é a sintaxe correta para declarar uma função em JavaScript?",
-      respostas: [
-        "function myFunction() {}",
-        "func myFunction() {}",
-        "def myFunction() {}"
-      ],
-      correta: 0
-    },
-    {
-      pergunta: "O que o operador '&&' faz em JavaScript?",
-      respostas: [
-        "Operador de comparação",
-        "Operador de negação",
-        "Operador lógico 'E' (AND)"
-      ],
-      correta: 2
-    }
-  ];
-  
+  {
+    pergunta: "Qual é o nome do protagonista de Naruto?",
+    respostas: [
+      "Sasuke Uchiha",
+      "Naruto Uzumaki",
+      "Sakura Haruno"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual é o nome do sensei de Naruto?",
+    respostas: [
+      "Kakashi Hatake",
+      "Iruka Umino",
+      "Jiraiya"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Quem é o líder da Akatsuki?",
+    respostas: [
+      "Pain",
+      "Itachi Uchiha",
+      "Madara Uchiha"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Qual é o nome do irmão mais velho de Sasuke?",
+    respostas: [
+      "Itachi Uchiha",
+      "Madara Uchiha",
+      "Obito Uchiha"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Quem é o pai de Naruto?",
+    respostas: [
+      "Minato Namikaze",
+      "Jiraiya",
+      "Hiruzen Sarutobi"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Qual é o nome da vila oculta da folha em Naruto?",
+    respostas: [
+      "Vila Oculta da Nuvem",
+      "Vila Oculta da Areia",
+      "Vila Oculta da Folha"
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual é o nome do bijuu que está selado dentro de Naruto?",
+    respostas: [
+      "Shukaku",
+      "Kurama",
+      "Matatabi"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Quem é o mentor de Jiraiya, Tsunade e Orochimaru?",
+    respostas: [
+      "Hiruzen Sarutobi",
+      "Madara Uchiha",
+      "Tobirama Senju"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Qual é a técnica de assinatura de Sasuke Uchiha?",
+    respostas: [
+      "Rasengan",
+      "Chidori",
+      "Rasenshuriken"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual é o nome do último filme de Naruto?",
+    respostas: [
+      "Naruto Shippuden: A Lenda da Pedra de Gelel",
+      "Naruto Shippuden: A Morte de Naruto",
+      "The Last: Naruto o Filme"
+    ],
+    correta: 2
+  }
+];
+
   //escopo do projeto
   const quiz = document.querySelector('#quiz')
   const template = document.querySelector('template')
